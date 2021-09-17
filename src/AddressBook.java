9520a866626e25bea9d85634a3649e6143ef5bea
Matthew Belanger <mbela1@ocdsb.ca>
2021-09-17 10:41:30 -0400

    First commit

---
+++ src/AddressBook.java
@@ -0,0 +1,22 @@
+import java.util.ArrayList;
+
+public class AddressBook {
+    private ArrayList<BuddyInfo> addressBook;
+
+    public AddressBook(){
+        this(new ArrayList<BuddyInfo>());
+    }
+
+    public AddressBook(ArrayList<BuddyInfo> addressBook) {
+        this.addressBook = addressBook;
+    }
+
+    public void addBuddy(BuddyInfo buddy){
+        this.addressBook.add(buddy);
+    }
+
+    public void removeBuddy(int index){
+        this.addressBook.remove(index);
+    }
+
+}
---
+++ src/BuddyInfo.java
@@ -0,0 +1,24 @@
+public class BuddyInfo {
+    private String name;
+    private String address;
+    private int phoneNumber;
+
+    public BuddyInfo(){
+        this("no name", "0", 0);
+    }
+
+    public BuddyInfo(String name, String address, int phoneNumber) {
+        this.name = name;
+        this.address = address;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public static void main(String[] args) {
+        BuddyInfo buddy = new BuddyInfo("Homer", "0", 0);
+        System.out.println("Hello " + buddy.getName());
+    }
+}
